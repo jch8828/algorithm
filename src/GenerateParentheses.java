c808dae39f17dde3aae0f388145e5169d9e3c08e
Joe <chenjz@yahoo.com>
2020-03-14 20:25:52 -0700

    Add generateParentheses and longestSubstingWithoutRepeatingChar

---
+++ src/GenerateParentheses.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Source:     https://leetcode.com/problems/generate-parentheses/
+ * Difficulty: Medium
+ * Note:
+ *     Given n pairs of parentheses, write a function to generate all combinations of well-formed
+ *     parentheses.
+ * Example:
+ *    given n = 3, a solution set is:
+ *
+ * [
+ *   "((()))",
+ *   "(()())",
+ *   "(())()",
+ *   "()(())",
+ *   "()()()"
+ * ]
+ *
+ */
+public class GenerateParentheses {
+  public static void main(String[] args) {
+    List<String> res = new ArrayList<>();
+    generateParentheses(2, 2, "", res);
+    System.out.println(res);
+
+  }
+
+  //  Don't use StringBuilder since it passes by reference
+  public static void generateParentheses(int left, int right, String path, List<String> res) {
+    if (left == 0 && right == 0) {
+      res.add(path.toString());
+      return;
+    }
+    // Need to check the left > 0
+    if (left > 0) generateParentheses(left-1, right, path + "(", res);
+    if (right > 0 && left < right) generateParentheses(left, right-1, path + ")", res);
+  }
+}
---
+++ src/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,56 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Source:     https://leetcode.com/problems/longest-substring-without-repeating-characters/
+ * Difficulty: Medium
+ * Note:
+ *    Given a string, find the length of the longest substring without repeating characters.
+ *
+ * Example 1:
+ *    Input: "abcabcbb"
+ *    Output: 3
+ *    Explanation: The answer is "abc", with the length of 3.
+ * Example 2:
+ *    Input: "bbbbb"
+ *    Output: 1
+ *    Explanation: The answer is "b", with the length of 1.
+ *
+ * Example 3:
+ *    Input: "pwwkew"
+ *    Output: 3
+ *    Explanation: The answer is "wke", with the length of 3.
+ *      Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ */
+public class LongestSubstringWithoutRepeatingCharacters {
+  public static void main(String[] args) {
+    System.out.printf("abcabcbb: %s\n", longestSubstring("abcabcbb"));
+    System.out.printf("bbbbb: %s\n", longestSubstring("bbbbb"));
+    System.out.printf("pwwkew: %s\n", longestSubstring("pwwkew"));
+
+  }
+
+  public static int longestSubstring(String s) {
+    if (s == null) throw new IllegalArgumentException();
+    if (s.length() < 1) return s.length();
+
+    Map<Character, Boolean> map = new HashMap<>(); // could use HashSet here, HashMap is better
+    int max = 0;
+    int start = 0;
+    int end = 0;
+    while (end < s.length()) {
+      max = Math.max(max, end - start); // calculate length before incrementing end
+      char c = s.charAt(end++);
+      if (map.containsKey(c)) {
+        while (s.charAt(start) != c) {
+          map.remove(s.charAt(start++));
+        }
+        start++; // move start index passing the original 'c' position
+      } else {
+        map.put(c, true);
+      }
+    }
+
+    return max;
+  }
+}
